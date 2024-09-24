d94d56b80f2ac00581d7ac371af8b6c977423b3a
UQ <131900984+unluckyquote@users.noreply.github.com>
2024-09-24 14:29:28 +0200

    Update generateToken.py

--- python/generateToken.py
+++ python/generateToken.py
@@ -8,5 +8,5 @@ if len(sys.argv) > 1:
     scope = 'user-read-currently-playing'
 
     # This way removes the need for a browser, it will instead give the URL to visit in the terminal
-    auth = SpotifyOAuth(scope=scope, open_browser=False)
+    auth = SpotifyOAuth(scope=scope, open_browser=True)
     token = auth.get_access_token()
